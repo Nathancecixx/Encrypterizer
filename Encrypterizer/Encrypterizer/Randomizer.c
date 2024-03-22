8c146faea52c055f9845a327c32d84f335fc3558
LIAM <lcabarle6383@conestogac.on.ca>
2024-03-21 20:21:28 -0400

    organized .h and .c files for randomizer

--- Encrypterizer/Encrypterizer/Randomizer.c
+++ Encrypterizer/Encrypterizer/Randomizer.c
@@ -23,17 +23,3 @@ void generateRandomKey(unsigned char key[], size_t size) {
 		}
 	}
 }
-
-int main(void) {
-
-	unsigned char key[16];
-	generateRandomKey(key, sizeof(key));
-
-	printf("Generated security key: ");
-	for (size_t i = 0; i < sizeof(key); i++) {
-		printf("%c", key[i]);
-	}
-	printf("\n");
-
-	return 0;
-}
--- Encrypterizer/Encrypterizer/main.c
+++ Encrypterizer/Encrypterizer/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "Randomizer.h"
 
 int main(int argc, char* argv) {
-	printf("Hello World!");
+	//testing for randomizer
+	unsigned char key[16];
+	generateRandomKey(key, sizeof(key));
+
+	printf("Generated security key: ");
+	for (size_t i = 0; i < sizeof(key); i++) {
+		printf("%c", key[i]);
+	}
+	printf("\n");
 	return 0;
 }
